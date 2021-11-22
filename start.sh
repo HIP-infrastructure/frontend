99266d038a3d4961c1c294088fcf2171e1c1ee24
Nathalie Casati <nathalie.casati@chuv.ch>
2021-11-22 14:24:32 +0000

    prod start script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $1 != "up" ]; then
+    docker-compose -f ./nextcloud-docker/docker-compose.yml down
+    docker-compose down
+else
+    #docker-compose -f ./nextcloud-docker/docker-compose.yml up caddy db
+    docker-compose -f ./nextcloud-docker/docker-compose.yml up -d
+    docker-compose up -d
+
+    # This will install the submodule hip into the static path of Nextcloud install
+    sudo cp -rf ./hip /mnt/nextcloud-dp/nextcloud/apps
+    sudo chown -R www-data:root /mnt/nextcloud-dp/nextcloud/apps/hips
+fi

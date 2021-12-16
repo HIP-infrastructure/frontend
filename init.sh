203c3af1f2bc9cf0b4d93dfa131496dfb21a7217
Manuel Spuhler <manuel.spuhler@chuv.ch>
2021-12-16 14:18:46 +0000

    dev stack

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
-
 #!/bin/bash
 
-# start both gateway & cache && Nextcloud 
-./nextcloud-docker/fix-contab.sh
+./nextcloud-docker/fix_crontab.sh
 docker-compose \
     -f ./nextcloud-docker/docker-compose.yml \
     up caddy db
--- start-dev.sh
+++ start-dev.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Get the HIP app Nextcloud app
-git submodule update hip
-cd hip
-git checkout dev
-cd ..
+NC_HIP_APP_FOLDER=/mnt/nextcloud-dp/nextcloud/apps/hip
+
+echo Copy Caddyfile to 
+cp Caddyfile.dev ./nextcloud-docker/caddy/Caddyfile
+
+# copy bootsrap files into nextcloud app folder
+sudo cp -rf ./hip $NC_HIP_APP_FOLDER
+sudo cp -rf ./hip/appinfo $NC_HIP_APP_FOLDER
+sudo cp -f ./hip/templates/index.dev.php $NC_HIP_APP_FOLDER/templates/index.php
+sudo chown -R www-data:root $NC_HIP_APP_FOLDER
 
 # start both gateway & cache && Nextcloud 
 docker-compose \
-    -f ./nextcloud-docker/docker-compose.yml \
-    -f ./docker-compose-dev.yml \
-    up -d
+    -f nextcloud-docker/docker-compose.yml \
+    $@
 
-# copy bundled app into nextcloud app folder
-sudo cp -rf ./hip /mnt/nextcloud-dp/nextcloud/apps
-sudo chown -R www-data:root /mnt/nextcloud-dp/nextcloud/apps/hip
+docker-compose \
+    -f docker-compose-dev.yml \
+    $@

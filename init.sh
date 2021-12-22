7eb0c0ec04e3cc00d7c81ef3cf2b5898428996a0
Manuel Spuhler <manuel.spuhler@chuv.ch>
2021-12-22 10:57:34 +0000

    typo

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # start both gateway & cache && Nextcloud 
-./nextcloud-docker/fix-contab.sh
+./nextcloud-docker/fix-crontab.sh
 docker-compose \
     -f ./nextcloud-docker/docker-compose.yml \
     up caddy db

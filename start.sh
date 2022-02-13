652ff4d1f6763525ba1885ef891ff27170fee4e7
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-02-13 17:27:43 +0000

    Updated start scripts

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NC_HIP_APP_FOLDER=/mnt/nextcloud-dp/nextcloud/apps/hip
+
 # Get the HIP app Nextcloud app
 git submodule update hip
 cd hip
@@ -9,11 +11,17 @@ cd ..
 # start both gateway & cache && Nextcloud 
 docker network create nextcloud-docker_frontend
 
+echo "Copy Caddyfile to ./nextcloud-docker/caddy/Caddyfile"
+cp Caddyfile ./nextcloud-docker/caddy/Caddyfile
+
 docker-compose \
     -f ./nextcloud-docker/docker-compose.yml \
+    up -d
+
+docker-compose \
     -f ./docker-compose.yml \
     up -d
 
 # copy bundled app into nextcloud app folder
 sudo cp -rf ./hip /mnt/nextcloud-dp/nextcloud/apps
-sudo chown -R www-data:root /mnt/nextcloud-dp/nextcloud/apps/hip
+sudo chown -R www-data:root $NC_HIP_APP_FOLDER
--- stop.sh
+++ stop.sh
@@ -3,5 +3,8 @@
 # stop both gateway & cache && Nextcloud 
 docker-compose \
     -f ./nextcloud-docker/docker-compose.yml \
+    stop
+
+docker-compose \
     -f ./docker-compose.yml \
     stop

9a7b819422987cb8c8ac3cb58315d891ceed5ae7
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-10-03 18:34:42 +0200

    fix: extras * removed

--- cron_restart_inotify.sh
+++ cron_restart_inotify.sh
@@ -4,7 +4,7 @@
 # This script is used temporarely to restart inotify service
 # install with 
 
-# 0 */2 * * * * * * * * /bin/sh /home/hipadmin/frontend/cron_restart_inotify.sh > /dev/null 2>&1
+# 0 */2 * * * /bin/sh /home/hipadmin/frontend/cron_restart_inotify.sh > /dev/null 2>&1
 
 docker-compose stop cron 
 docker-compose start cron

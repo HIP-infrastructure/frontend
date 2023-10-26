f7c2e9e8910c0164eead47ea2c23d581773f6bd5
Sami Perrin <sami.perrin@chuv.ch>
2023-10-26 14:09:41 +0200

    dev: docker + caddy config

--- pm2/ecosystem.ghostfs.config.js
+++ pm2/ecosystem.ghostfs.config.js
@@ -10,6 +10,11 @@ const gunicorn = which("gunicorn");
 const env = dotenv.config({ path: relative("../.env") }).parsed;
 
 const data = "/mnt/nextcloud-dp/nextcloud/data";
+const collab_data = "/mnt/collab";
+const port = "3444"
+const auth_port = "3445"
+const collab_port = "3447"
+const collab_auth_port = "3448"
 const host = "0.0.0.0";
 const suffix = "files";
 const cert = env.GHOSTFS_CERT;
@@ -22,7 +27,20 @@ module.exports = {
       cwd: relative('../ghostfs'),
       name: 'ghostfs',
       watch: false,
-      args: `-s /bin/sh www-data -c "./GhostFS --server --root ${data} --bind ${host} --suffix ${suffix} --key ${key} --cert ${cert}"`,
+      env: {
+        "LD_LIBRARY_PATH": "/home/dsr_adm/app-in-browser/libssl/out"
+      },
+      args: `-s /bin/sh www-data -c "./GhostFSLauncher.sh --server --root ${data} --bind ${host} --suffix ${suffix} --key ${key} --cert ${cert} --port ${port} --auth-port ${auth_port}"`,
+    },
+    {
+      script: "su",
+      cwd: relative('../ghostfs'),
+      name: 'ghostfs_collab',
+      watch: false,
+      env: {
+        "LD_LIBRARY_PATH": "/home/dsr_adm/app-in-browser/libssl/out"
+      },
+      args: `-s /bin/sh www-data -c "./GhostFSLauncher.sh --server --root ${collab_data} --bind ${host} --suffix ${suffix} --key ${key} --cert ${cert}  --port ${collab_port} --auth-port ${collab_auth_port}"`,
     },
     {
       script: gunicorn,
@@ -30,6 +48,9 @@ module.exports = {
       name: 'gunicorn_auth_backend',
       cwd: relative('../ghostfs'),
       watch: relative('../ghostfs/auth_backend'),
+      env: {
+        "LD_LIBRARY_PATH": "/home/dsr_adm/app-in-browser/libssl/out"
+      },
       interpreter: 'python3' 
     }
   ],

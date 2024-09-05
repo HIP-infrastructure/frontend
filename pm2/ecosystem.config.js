4b4826d53b75c7c08f060e04bcaa36915b49f0ff
Sami <sami.perrin@chuv.ch>
2024-09-05 12:07:51 +0200

    whitelist new env vars

--- pm2/ecosystem.config.js
+++ pm2/ecosystem.config.js
@@ -8,7 +8,12 @@ const relative = (...dir) => path.resolve(__dirname, ...dir);
 const env = dotenv.config({ path: relative("../.env") }).parsed || {};
 
 // Merge .env variables with process.env, with process.env taking precedence
-const whitelist = ['REMOTE_APP_API', 'COLLAB_REMOTE_APP_API', 'EXT_CADDY_BASE_DOMAIN', 'EXT_CADDY_KEYCLOAK_DOMAIN', 'EXT_CADDY_KEYCLOAK_LISTEN_ENABLED']; 
+const whitelist = [
+  'REMOTE_APP_API', 'COLLAB_REMOTE_APP_API', 
+  'EXT_CADDY_BASE_DOMAIN', 
+  'EXT_CADDY_KEYCLOAK_DOMAIN', 'EXT_CADDY_KEYCLOAK_LISTEN_ENABLED',
+  'EXT_CADDY_APP_IN_BROWSER_DOMAIN', 'EXT_CADDY_APP_IN_BROWSER_LISTEN_ENABLED',
+]; 
 whitelist.forEach(key => {
   if (process.env[key]) {
     env[key] = process.env[key];
